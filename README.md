6bd83ad932c9b1eba7654f8ff45545c46b409602
Pokorny72 <rafalpokorny82@gmail.com>
2022-03-04 13:55:01 +0000

    Add extras for qssi-user 11 RKQ1.200826.002 V12.5.6.0.RKHMIXM release-keys
    Signed-off-by: Pokorny72 <rafalpokorny82@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.200826.002 V12.5.6.0.RKHMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: kona
+- Codename: alioth
+- Brand: Redmi
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.200826.002
+- Incremental: V12.5.6.0.RKHMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.200826.002-V12.5.6.0.RKHMIXM-release-keys
+- Repo: redmi_alioth_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/data-app/Photos/Photos.apk.* 2>/dev/null >> product/data-app/Photos/Photos.apk
+rm -f product/data-app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat vendor_bootimg/04_dtbdump_*qA5s.dtb.* 2>/dev/null >> vendor_bootimg/04_dtbdump_*qA5s.dtb
+rm -f vendor_bootimg/04_dtbdump_*qA5s.dtb.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat modem/image/sdx55m/qdsp6sw.mbn.* 2>/dev/null >> modem/image/sdx55m/qdsp6sw.mbn
+rm -f modem/image/sdx55m/qdsp6sw.mbn.* 2>/dev/null
+cat vendor/camera/mimoji/model2.zip.* 2>/dev/null >> vendor/camera/mimoji/model2.zip
+rm -f vendor/camera/mimoji/model2.zip.* 2>/dev/null
+cat vendor/lib64/librelight_only.so.* 2>/dev/null >> vendor/lib64/librelight_only.so
+rm -f vendor/lib64/librelight_only.so.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null >> system/system/priv-app/MiuiGallery/MiuiGallery.apk
+rm -f system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
+cat system/system/app/MiGameService/MiGameService.apk.* 2>/dev/null >> system/system/app/MiGameService/MiGameService.apk
+rm -f system/system/app/MiGameService/MiGameService.apk.* 2>/dev/null
+cat system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null >> system/system/lib64/librelight_only_extraphoto.so
+rm -f system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat .git/objects/pack/pack-3c1359150b461fda3e871d331816a986667357cb.pack.* 2>/dev/null >> .git/objects/pack/pack-3c1359150b461fda3e871d331816a986667357cb.pack
+rm -f .git/objects/pack/pack-3c1359150b461fda3e871d331816a986667357cb.pack.* 2>/dev/null
+cat .git/objects/pack/pack-7de25352b55337fd11785941cfda2e1a12d832eb.pack.* 2>/dev/null >> .git/objects/pack/pack-7de25352b55337fd11785941cfda2e1a12d832eb.pack
+rm -f .git/objects/pack/pack-7de25352b55337fd11785941cfda2e1a12d832eb.pack.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
